33f7939098dc883a73e5680bc1d5ac9f2aa19944
khadkacrystal23 <khadkacrystal23@gmail.com>
2022-11-24 12:33:43 +0545

    completed gui of view bed

---
+++ src/view/test.java
@@ -0,0 +1,36 @@
+package view;
+
+import javax.swing.JOptionPane;
+
+import controller.BedController;
+import models.Bed;
+
+public class test {
+    public static void main(String[] args) {
+        // try {
+        //     Bed b1 = new Bed(0, 1, "manual", "42 inch", "General");
+        //     BedController bc= new BedController();
+        //     int result = bc.insertdetails(b1);
+        //     if(result>0){
+        //         JOptionPane.showMessageDialog(null, "Bed Added Successfully");
+        //     }
+        //     else{
+        //         JOptionPane.showMessageDialog(null, "Bed can't be added");
+                
+        //     }
+            
+        // } catch (Exception e) {
+        //     // TODO: handle exception
+        //     JOptionPane.showMessageDialog(null, "e");
+        
+        // }
+        
+        
+        try {
+            
+        } catch (Exception e) {
+                JOptionPane.showMessageDialog(null, "e");
+            
+        }
+    }
+}
