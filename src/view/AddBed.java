5a920d25ebd2a48397468079fdf0cb900191b6b2
khadkacrystal23 <khadkacrystal23@gmail.com>
2022-11-30 12:39:47 +0545

    done adjustment

--- src/view/AddBed.java
+++ src/view/AddBed.java
@@ -10,8 +10,8 @@ import javax.swing.JOptionPane;
 import javax.swing.table.DefaultTableModel;
 import javax.swing.table.TableModel;
 
-import controller.BedController;
-import models.Bed;
+import models.*;
+import controller.*;
 
 /**
  *
