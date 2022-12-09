adb28dc3cbeeeba7a8015b94f04cccca0dbc9d7e
Rajeev9813 <razeev.mahat2058@gmail.com>
2022-12-09 16:53:20 +0545

    Updating Final Ui for Sign in

--- src/view/Login.java
+++ src/view/Login.java
@@ -42,6 +42,7 @@ public class Login extends javax.swing.JFrame {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
+        Role = new javax.swing.ButtonGroup();
         jPanel1 = new javax.swing.JPanel();
         jLabel2 = new javax.swing.JLabel();
         jLabel3 = new javax.swing.JLabel();
@@ -52,6 +53,11 @@ public class Login extends javax.swing.JFrame {
         jButton1 = new javax.swing.JButton();
         loginbtn = new javax.swing.JButton();
         jButton3 = new javax.swing.JButton();
+        jButton2 = new javax.swing.JButton();
+        jLabel7 = new javax.swing.JLabel();
+        jLabel6 = new javax.swing.JLabel();
+        jRadioButton1 = new javax.swing.JRadioButton();
+        jRadioButton2 = new javax.swing.JRadioButton();
         jLabel1 = new javax.swing.JLabel();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
@@ -59,72 +65,117 @@ public class Login extends javax.swing.JFrame {
         setResizable(false);
 
         jPanel1.setBackground(java.awt.Color.gray);
-        jPanel1.setLayout(new AbsoluteLayout());
+        jPanel1.setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
 
-        jLabel2.setFont(new java.awt.Font("Segoe UI", 1, 24)); // NOI18N
+        jLabel2.setFont(new java.awt.Font("Tw Cen MT Condensed Extra Bold", 1, 48)); // NOI18N
         jLabel2.setText("SIGN IN");
-        jPanel1.add(jLabel2, new AbsoluteConstraints(620, 40, 190, -1));
+        jLabel2.setBorder(new javax.swing.border.LineBorder(new java.awt.Color(0, 255, 255), 4, true));
+        jPanel1.add(jLabel2, new org.netbeans.lib.awtextra.AbsoluteConstraints(440, 20, 190, -1));
 
-        jLabel3.setFont(new java.awt.Font("Segoe UI", 1, 24)); // NOI18N
+        jLabel3.setFont(new java.awt.Font("Perpetua Titling MT", 1, 24)); // NOI18N
         jLabel3.setText("Access Your Account");
-        jPanel1.add(jLabel3, new AbsoluteConstraints(620, 90, 260, -1));
+        jLabel3.setBorder(new javax.swing.border.LineBorder(new java.awt.Color(0, 255, 255), 4, true));
+        jPanel1.add(jLabel3, new org.netbeans.lib.awtextra.AbsoluteConstraints(360, 90, 370, 40));
 
-        jLabel4.setFont(new java.awt.Font("Segoe UI", 0, 24)); // NOI18N
+        jLabel4.setFont(new java.awt.Font("Rockwell Extra Bold", 1, 24)); // NOI18N
         jLabel4.setText("Email");
-        jPanel1.add(jLabel4, new AbsoluteConstraints(530, 150, 180, -1));
+        jPanel1.add(jLabel4, new org.netbeans.lib.awtextra.AbsoluteConstraints(360, 130, 180, -1));
 
         eText.setBackground(java.awt.Color.black);
-        eText.setFont(new java.awt.Font("Segoe UI", 0, 24)); // NOI18N
+        eText.setFont(new java.awt.Font("Times New Roman", 1, 18)); // NOI18N
         eText.setForeground(java.awt.Color.white);
-        jPanel1.add(eText, new AbsoluteConstraints(530, 190, 290, 40));
+        jPanel1.add(eText, new org.netbeans.lib.awtextra.AbsoluteConstraints(360, 170, 290, 40));
 
-        jLabel5.setFont(new java.awt.Font("Segoe UI", 0, 24)); // NOI18N
+        jLabel5.setFont(new java.awt.Font("Rockwell Extra Bold", 1, 24)); // NOI18N
         jLabel5.setText("Password");
-        jPanel1.add(jLabel5, new AbsoluteConstraints(530, 250, 180, -1));
+        jPanel1.add(jLabel5, new org.netbeans.lib.awtextra.AbsoluteConstraints(360, 220, 180, -1));
 
         pText.setBackground(java.awt.Color.black);
-        pText.setFont(new java.awt.Font("Segoe UI", 0, 24)); // NOI18N
+        pText.setFont(new java.awt.Font("Tw Cen MT Condensed Extra Bold", 1, 18)); // NOI18N
         pText.setForeground(java.awt.Color.white);
         pText.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
                 pTextActionPerformed(evt);
             }
         });
-        jPanel1.add(pText, new AbsoluteConstraints(530, 290, 300, 40));
+        jPanel1.add(pText, new org.netbeans.lib.awtextra.AbsoluteConstraints(360, 250, 300, 40));
 
-        jButton1.setFont(new java.awt.Font("Segoe UI", 1, 18)); // NOI18N
-        jButton1.setText("Forgot Password");
-        jButton1.setBorder(null);
+        jButton1.setBackground(new java.awt.Color(0, 0, 0));
+        jButton1.setFont(new java.awt.Font("Palatino Linotype", 1, 24)); // NOI18N
+        jButton1.setForeground(new java.awt.Color(0, 255, 255));
+        jButton1.setText("SIGN UP");
+        jButton1.setBorder(new javax.swing.border.LineBorder(new java.awt.Color(255, 255, 0), 4, true));
         jButton1.setBorderPainted(false);
         jButton1.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
                 jButton1ActionPerformed(evt);
             }
         });
-        jPanel1.add(jButton1, new AbsoluteConstraints(580, 350, 180, -1));
+        jPanel1.add(jButton1, new org.netbeans.lib.awtextra.AbsoluteConstraints(400, 460, 180, -1));
 
-        loginbtn.setBackground(new java.awt.Color(204, 204, 204));
-        loginbtn.setFont(new java.awt.Font("Segoe UI", 0, 24)); // NOI18N
+        loginbtn.setBackground(new java.awt.Color(0, 0, 0));
+        loginbtn.setFont(new java.awt.Font("Perpetua Titling MT", 1, 24)); // NOI18N
+        loginbtn.setForeground(new java.awt.Color(0, 255, 255));
         loginbtn.setText("Login");
+        loginbtn.setBorder(new javax.swing.border.LineBorder(new java.awt.Color(255, 0, 51), 4, true));
         loginbtn.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
                 loginbtnActionPerformed(evt);
             }
         });
-        jPanel1.add(loginbtn, new AbsoluteConstraints(535, 440, 170, 40));
+        jPanel1.add(loginbtn, new org.netbeans.lib.awtextra.AbsoluteConstraints(760, 250, 170, 40));
 
+        jButton3.setBackground(new java.awt.Color(0, 0, 0));
         jButton3.setFont(new java.awt.Font("Segoe UI", 1, 24)); // NOI18N
         jButton3.setForeground(new java.awt.Color(255, 51, 51));
         jButton3.setText("About us");
+        jButton3.setBorder(new javax.swing.border.LineBorder(new java.awt.Color(255, 0, 51), 4, true));
         jButton3.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
                 jButton3ActionPerformed(evt);
             }
         });
-        jPanel1.add(jButton3, new AbsoluteConstraints(405, 520, 160, -1));
+        jPanel1.add(jButton3, new org.netbeans.lib.awtextra.AbsoluteConstraints(420, 510, 140, -1));
+
+        jButton2.setBackground(new java.awt.Color(0, 0, 0));
+        jButton2.setFont(new java.awt.Font("Palatino Linotype", 1, 24)); // NOI18N
+        jButton2.setForeground(new java.awt.Color(0, 255, 255));
+        jButton2.setText("Forgot Password ?");
+        jButton2.setBorder(new javax.swing.border.LineBorder(new java.awt.Color(255, 0, 51), 4, true));
+        jButton2.setBorderPainted(false);
+        jButton2.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton2ActionPerformed(evt);
+            }
+        });
+        jPanel1.add(jButton2, new org.netbeans.lib.awtextra.AbsoluteConstraints(740, 300, 230, -1));
+
+        jLabel7.setFont(new java.awt.Font("Stencil", 1, 24)); // NOI18N
+        jLabel7.setText("PLEASE SELECT YOUR ROLE");
+        jLabel7.setBorder(new javax.swing.border.LineBorder(new java.awt.Color(255, 255, 0), 4, true));
+        jPanel1.add(jLabel7, new org.netbeans.lib.awtextra.AbsoluteConstraints(340, 300, 360, -1));
+
+        jLabel6.setFont(new java.awt.Font("Perpetua Titling MT", 1, 18)); // NOI18N
+        jLabel6.setForeground(new java.awt.Color(204, 0, 0));
+        jLabel6.setText("Don't have an account ?");
+        jPanel1.add(jLabel6, new org.netbeans.lib.awtextra.AbsoluteConstraints(350, 420, -1, -1));
+
+        Role.add(jRadioButton1);
+        jRadioButton1.setFont(new java.awt.Font("Perpetua Titling MT", 1, 24)); // NOI18N
+        jRadioButton1.setForeground(new java.awt.Color(255, 0, 102));
+        jRadioButton1.setText("ADMIN");
+        jRadioButton1.setBorder(new javax.swing.border.LineBorder(new java.awt.Color(0, 255, 204), 4, true));
+        jPanel1.add(jRadioButton1, new org.netbeans.lib.awtextra.AbsoluteConstraints(380, 350, -1, -1));
+
+        Role.add(jRadioButton2);
+        jRadioButton2.setFont(new java.awt.Font("Perpetua Titling MT", 1, 24)); // NOI18N
+        jRadioButton2.setForeground(new java.awt.Color(255, 0, 102));
+        jRadioButton2.setText("USER");
+        jRadioButton2.setBorder(new javax.swing.border.LineBorder(new java.awt.Color(0, 255, 204), 4, true));
+        jPanel1.add(jRadioButton2, new org.netbeans.lib.awtextra.AbsoluteConstraints(530, 350, -1, -1));
 
         jLabel1.setIcon(new javax.swing.ImageIcon(getClass().getResource("/view/img/Login_pic.png"))); // NOI18N
-        jPanel1.add(jLabel1, new AbsoluteConstraints(0, 0, -1, 570));
+        jPanel1.add(jLabel1, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 0, -1, 570));
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
@@ -178,6 +229,10 @@ public class Login extends javax.swing.JFrame {
         // TODO add your handling code here:
     }//GEN-LAST:event_pTextActionPerformed
 
+    private void jButton2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton2ActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_jButton2ActionPerformed
+
     /**
      * @param args the command line arguments
      */
@@ -214,15 +269,21 @@ public class Login extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.ButtonGroup Role;
     private javax.swing.JTextField eText;
     private javax.swing.JButton jButton1;
+    private javax.swing.JButton jButton2;
     private javax.swing.JButton jButton3;
     private javax.swing.JLabel jLabel1;
     private javax.swing.JLabel jLabel2;
     private javax.swing.JLabel jLabel3;
     private javax.swing.JLabel jLabel4;
     private javax.swing.JLabel jLabel5;
+    private javax.swing.JLabel jLabel6;
+    private javax.swing.JLabel jLabel7;
     private javax.swing.JPanel jPanel1;
+    private javax.swing.JRadioButton jRadioButton1;
+    private javax.swing.JRadioButton jRadioButton2;
     private javax.swing.JButton loginbtn;
     private javax.swing.JTextField pText;
     // End of variables declaration//GEN-END:variables
