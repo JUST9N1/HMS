438b3fef51fbadf1791de01b72ed7710c6622da1
Rajeev9813 <razeev.mahat2058@gmail.com>
2022-12-24 19:02:21 +0545

    updating UI

--- src/view/addApointment.java
+++ src/view/addApointment.java
@@ -5,7 +5,7 @@ package view;
  * Click nbfs://nbhost/SystemFileSystem/Templates/GUIForms/JFrame.java to edit this template
  */
 
-
+//importing libraries
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
